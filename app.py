a16517d61b882fb7c1e223babef15d6da29da11b
Tomáš Oross <tomi.oross@gmail.com>
2022-07-12 22:02:40 +0200

    Added math library

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 from datetime import datetime
 from datetime import timedelta
 import json
+import math
 
 from flask import Flask, render_template, request, redirect, url_for
 
